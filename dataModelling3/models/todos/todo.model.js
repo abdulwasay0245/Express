c431203f3052cb96b4cb3f64683883dc410a50db
Abdul Wasay <abdulwasay0245@gmail.com>
2026-03-28 10:00:29 +0500

    data_modelling

---
+++ dataModelling3/models/todos/todo.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const TodoSchema = new mongoose.Schema(
+    {
+        content: {
+            type: String,
+            required: true,
+        },
+        isCompleted: {
+            type: Boolean,
+            default: false,
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        subTodo: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'SubTodo',
+            },
+        ],
+    },
+    {timestamps:true}
+)
+
+export const Todo = mongoose.model('Todo',TodoSchema)
