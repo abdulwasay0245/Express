caf8503e1b1f5d997bd911310f8c5f61f1261fcd
Abdul Wasay <abdulwasay0245@gmail.com>
2026-04-24 16:22:31 +0500

    add user and video model with jwt

--- mainproject4/src/models/video.model.js
+++ mainproject4/src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema({
 
@@ -38,5 +39,5 @@ const videoSchema = new Schema({
     
 
 },{timestamps:true})
-
+videoSchema.plugin(mongooseAggregatePaginate)
 export const video = mongoose.model("Video", videoSchema)
